6fe3447c4bd74855c4825183c75fd4aaa3a47315
Parallels <menglanj@andrew.cmu.edu>
2018-01-20 22:10:18 -0500

    Initial commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,242 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Rizwan on 16/06/17.
+//  Copyright © 2017 Rizwan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var timer = Timer()
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    var qrCodeFrameView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            fatalError("No vidoe device found")
+        }
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            
+            // Set the input devcie on the capture session
+            captureSession?.addInput(input)
+            
+            // Get an instance of ACCapturePhotoOutput class
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.isHighResolutionCaptureEnabled = false
+            
+            // Set the output on the capture session
+            captureSession?.addOutput(capturePhotoOutput!)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the input device
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            //start video capture
+            captureSession?.startRunning()
+            
+            messageLabel.isHidden = false
+            messageLabel.textColor = UIColor.white
+            messageLabel.text = "SECURITY CAM - PennApps"
+            
+            takePhotoAtInterval(interval: -1.0)
+        } catch {
+            //If any error occurs, simply print it out
+            print(error)
+            return
+        }
+    
+    }
+
+    override func viewDidLayoutSubviews() {
+        videoPreviewLayer?.frame = view.bounds
+        if let previewLayer = videoPreviewLayer ,(previewLayer.connection?.isVideoOrientationSupported)! {
+            previewLayer.connection?.videoOrientation = UIApplication.shared.statusBarOrientation.videoOrientation ?? .portrait
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func sendPhoto() {
+        // Make sure capturePhotoOutput is valid
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        
+        // Get an instance of AVCapturePhotoSettings class
+        let photoSettings = AVCapturePhotoSettings()
+        
+        // Set photo settings for our need
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = false
+        photoSettings.flashMode = .off
+        
+        // Call capturePhoto method by passing our photo settings and a delegate implementing AVCapturePhotoCaptureDelegate
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    func takePhotoAtInterval(interval: Double) {
+        if (interval < 0) {
+            // Take one photo
+            sendPhoto()
+        } else {
+            // Take one photo per interval seconds
+            timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector:#selector(self.sendPhoto), userInfo: nil, repeats: true)
+        }
+    }
+}
+
+extension ViewController : AVCapturePhotoCaptureDelegate {
+    func submitPhoto(photo: Data) {
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let capturedImage = UIImage.init(data: photo , scale: 1.0)!
+        let parameters = ["image": UIImageJPEGRepresentation(capturedImage, 1) ] as! Dictionary<String, Data>
+        
+        //create the url with URL
+        let url = URL(string: "http://myServerName.com/api")! //change the url
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch {
+            print(error)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            print(String(describing: error))
+        })
+        task.resume()
+    }
+    
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput,
+                 didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?,
+                 previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?,
+                 resolvedSettings: AVCaptureResolvedPhotoSettings,
+                 bracketSettings: AVCaptureBracketedStillImageSettings?,
+                 error: Error?) {
+        // Make sure we get some photo sample buffer
+        guard error == nil,
+            let photoSampleBuffer = photoSampleBuffer else {
+            print("Error capturing photo: \(String(describing: error))")
+            return
+        }
+        
+        // Convert photo same buffer to a jpeg image data by using AVCapturePhotoOutput
+        guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+            return
+        }
+        
+        let capturedImage = UIImage.init(data: imageData , scale: 1.0)!
+        if let imageData = capturedImage.jpeg(.lowest) {
+            // Post to API
+            submitPhoto(photo: imageData)
+        }
+
+        /* Initialise an UIImage with our image data
+        let capturedImage = UIImage.init(data: imageData , scale: 1.0)
+        if let image = capturedImage {
+            // Save our captured image to photos album
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }*/
+    }
+}
+
+extension ViewController : AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
+                       didOutput metadataObjects: [AVMetadataObject],
+                       from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.isHidden = true
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.isHidden = false
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+}
+
+extension UIInterfaceOrientation {
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portraitUpsideDown: return .portraitUpsideDown
+        case .landscapeRight: return .landscapeRight
+        case .landscapeLeft: return .landscapeLeft
+        case .portrait: return .portrait
+        default: return nil
+        }
+    }
+}
+
+extension UIImage {
+    enum JPEGQuality: CGFloat {
+        case lowest  = 0
+        case low     = 0.25
+        case medium  = 0.5
+        case high    = 0.75
+        case highest = 1
+    }
+    
+    /// Returns the data for the specified image in JPEG format.
+    /// If the image object’s underlying image data has been purged, calling this function forces that data to be reloaded into memory.
+    /// - returns: A data object containing the JPEG data, or nil if there was a problem generating the data. This function may return nil if the image has no data or if the underlying CGImageRef contains data in an unsupported bitmap format.
+    func jpeg(_ quality: JPEGQuality) -> Data? {
+        return UIImageJPEGRepresentation(self, quality.rawValue)
+    }
+}
